9b5380ed5dd698177f24c8eb6a5624b8d8db0f4b
Petros-Code <petros-code@proton.me>
2026-02-27 15:33:43 +0100

    feat-Changement de brevo api http V4 -> V2

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,25 +1,26 @@
-import * as Brevo from '@getbrevo/brevo'
+import Brevo from '@getbrevo/brevo'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const apiInstance = new Brevo.TransactionalEmailsApi()
-apiInstance.authentications['apiKey'].apiKey = process.env.MAIL_PASS
-
 export const sendVerificationEmail = async (to, verify_token) => {
-  const email = new Brevo.SendSmtpEmail()
+  const client = Brevo.ApiClient.instance
+  client.authentications['api-key'].apiKey = process.env.MAIL_PASS
 
-  email.subject = 'Vérifie ton compte CyberMapp'
-  email.to = [{ email: to }]
-  email.sender = { email: process.env.MAIL_USER, name: 'CyberMapp' }
-  email.htmlContent = `
-    <h2>Bienvenue sur CyberMapp !</h2>
-    <p>Clique sur le lien ci-dessous pour vérifier ton email :</p>
-    <a href="${process.env.CLIENT_URL}/verify/${verify_token}">
-      Vérifier mon email
-    </a>
-  `
+  const apiInstance = new Brevo.TransactionalEmailsApi()
 
-  await apiInstance.sendTransacEmail(email)
+  await apiInstance.sendTransacEmail({
+    subject: 'Vérifie ton compte CyberMapp',
+    to: [{ email: to }],
+    sender: { email: process.env.MAIL_USER, name: 'CyberMapp' },
+    htmlContent: `
+      <h2>Bienvenue sur CyberMapp !</h2>
+      <p>Clique sur le lien ci-dessous pour vérifier ton email :</p>
+      <a href="${process.env.CLIENT_URL}/verify/${verify_token}">
+        Vérifier mon email
+      </a>
+    `,
+  })
+
   console.log('✅ Email envoyé à', to)
 }
